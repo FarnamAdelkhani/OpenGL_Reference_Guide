17b9e723b4635afa19dfc3e98bd0494b7ecf1f06
Farnam Adelkhani <Farnam7@gmail.com>
2020-09-09 21:31:59 -0700

    Added uniform for shader color and color transition

--- src/application.cpp
+++ src/application.cpp
@@ -135,6 +135,8 @@ int main(void)
     //Make the window's context current 
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Error!";
@@ -181,15 +183,29 @@ int main(void)
     unsigned int shader = createShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 0.2f));
+
     //glBindBuffer(GL_ARRAY_BUFFER, buffer);
 
+    float r = 0.0f;
+    float increment = 0.05f;
     // Loop until the user closes the window j
     while (!glfwWindowShouldClose(window))
     {
         // Render here 
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 0.2f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        
+        r += increment;
 
         // Swap front and back buffers 
         glfwSwapBuffers(window);
